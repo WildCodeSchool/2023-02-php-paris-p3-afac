98eac0a18789b7dde880d38b0c82b89697ad8226
ju-wd <julien.poincloux@gmail.com>
2023-06-14 14:25:31 +0200

    maj #2 PR initialisation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -58,7 +58,6 @@ class User implements PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
