d51dae691bc94c3b34c553af6ad4b49c859d5004
ju-wd <julien.poincloux@gmail.com>
2023-07-04 17:25:25 +0200

    modification WorkFixtures

--- src/DataFixtures/WorkFixtures.php
+++ src/DataFixtures/WorkFixtures.php
@@ -27,7 +27,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         Scène d\'apparence paisible ?',
             'reference' => '40FI79',
             'format' => '20 X 14',
-            'picture' => 'Cheminee_40FI79.ca49c5a5.jpg',
+            'picture' => '1.ca49c5a5.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -48,7 +48,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         pour le séchage du sucre?',
             'reference' => '40FI78',
             'format' => '15 X 13.5',
-            'picture' => 'UsineBelAir_40FI78.611ce888.jpg',
+            'picture' => '2.611ce888.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -71,7 +71,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         pour le tressage de sacs, destinés ensuite à transporter le sucre produit.',
             'reference' => '40FI106',
             'format' => '19.5 X 16.5',
-            'picture' => 'AD974_40FI106.3d9d2477.jpg',
+            'picture' => '3.3d9d2477.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -94,7 +94,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         Sur sa fiche d\'engagement, il était recensé comme tailleur',
             'reference' => '40FI91',
             'format' => '19.5 X 11',
-            'picture' => 'FRAD974_40FI91.da9cf76f.jpg',
+            'picture' => '4.da9cf76f..jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -107,7 +107,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         Le Canova, on le dit âgé de 17 ans',
             'reference' => '40FI76',
             'format' => '19.5 X 8.5',
-            'picture' => 'FRAD974_40FI90.11418c17.jpg',
+            'picture' => '5.11418c17.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -122,7 +122,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         avant 1848 pour les esclaves,par le "Code noir" de 1723. ',
             'reference' => '40FI76',
             'format' => '18 X 13',
-            'picture' => 'MORTIERdeTREVISE_caffrine1861.c2ec0a6f.jpg',
+            'picture' => '6.c2ec0a6f.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -134,7 +134,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         Son foulard noué sur la tête est taillé dans la même toile.',
             'reference' => '40FI52',
             'format' => '18 X 12',
-            'picture' => 'FRAD974_40FI52.46f74207.jpg',
+            'picture' => '7.46f74207.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -147,7 +147,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         ou les abords du camp des travailleurs.',
             'reference' => '40Fi60',
             'format' => '26 X 16.5',
-            'picture' => 'Jamali.aecddd12.jpg',
+            'picture' => '8.aecddd12.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -166,7 +166,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         la silhouette d\'une cheminée d\'usine, peut-être celle de Bel-Air, au Tampon.',
             'reference' => '40FI55',
             'format' => '19 X 11.5',
-            'picture' => 'FRAD974_40FI55.cbca2102.jpg',
+            'picture' => '9.cbca2102.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -176,7 +176,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
             'description' => 'La cascade alimente un bassin à proximité d\'un affluent de la rivière d\'Abord',
             'reference' => '40Fi77',
             'format' => '15 X 9,5',
-            'picture' => 'FRAD974_40FI77.88dfe062.jpg',
+            'picture' => '10.88dfe062.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -191,7 +191,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         Bellecombe avait commandité l\'expédition.',
             'reference' => '40Fi105bis',
             'format' => '18 X 24',
-            'picture' => 'FRAD974_40FI105bis.deb9b957.jpg',
+            'picture' => '11.deb9b957.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -204,7 +204,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         à cause de l\'extension des champs cultivés en cannes à sucre.',
             'reference' => '40Fi105bis',
             'format' => '19 X 23',
-            'picture' => 'caille.f2e834c9.jpg',
+            'picture' => '12.039c5bd2.jpg',
             'technique' => 'aquarelle',
             'artist' => 'Hippolyte'
         ],
@@ -215,7 +215,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         sans doute dans la maison des Casernes.',
             'reference' => '40Fi105bis',
             'format' => '40Fi75',
-            'picture' => 'FRAD974_40FI75.32b9295e.jpg',
+            'picture' => '13.32b9295e.jpg',
             'technique' => 'dessin',
             'artist' => 'Hippolyte'
         ],
@@ -228,7 +228,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         dans la propriété des Kerveguen.',
             'reference' => '40Fi105bis',
             'format' => '40Fi74',
-            'picture' => 'FRAD974_40FI74.f7c59c36.jpg',
+            'picture' => '14.f7c59c36.jpg',
             'technique' => 'dessin',
             'artist' => 'Hippolyte'
         ],
@@ -242,7 +242,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         Janvier est en pleine période cyclonique: est-ce le cas ici? ',
             'reference' => '40FI53.2',
             'format' => '30 X 20',
-            'picture' => 'lapli Tampon.0db53700.jpg',
+            'picture' => '15.0db53700.jpg',
             'technique' => 'dessin',
             'artist' => 'Hippolyte'
         ],
@@ -253,7 +253,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         vivement mettre la racine de ce plant (?) comme à Madras!" ',
             'reference' => '40FI59',
             'format' => '20 X 12.5',
-            'picture' => 'MORTIER.de.TREVISE_Mr.Bourraye.dans.jardin.Casernes_1861.076d6c20.jpg',
+            'picture' => '16.076d6c20.jpg',
             'technique' => 'dessin',
             'artist' => 'Hippolyte'
         ],
@@ -269,7 +269,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         au tiers du parcours, entre La Plaine des Cafres et Saint-Pierre.',
             'reference' => '40Fi72',
             'format' => '8 X 15.5',
-            'picture' => 'AD974_40FI72-ChanvertGolo.aa3a214c.jpg',
+            'picture' => '17.aa3a214c.jpg',
             'technique' => 'dessin',
             'artist' => 'Hippolyte'
         ],
@@ -283,7 +283,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         il y eut un atelier sur l\'Etablissement du Tampon.',
             'reference' => '40Fi108',
             'format' => '14.5 X 19.5',
-            'picture' => 'Mamsellemini_40FI108.fc3c1475.jpg',
+            'picture' => '18.fc3c1475.jpg',
             'technique' => 'dessin',
             'artist' => 'Hippolyte'
         ],
@@ -300,7 +300,7 @@ class WorkFixtures extends Fixture implements DependentFixtureInterface
                         Tec Tec, Langoutil, et Malheur sont des noms de mules.',
             'reference' => '40FI73',
             'format' => '6.5 X 15.5',
-            'picture' => 'trevise-cannes_1861.293b0662.jpg',
+            'picture' => '19.293b0662.jpg',
             'technique' => 'dessin',
             'artist' => 'Hippolyte'
         ],
