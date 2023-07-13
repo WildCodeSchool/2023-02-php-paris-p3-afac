c1a9921261e5519da0d8f0650f19b3e0c77d7720
Loic Njo <loicnjo@gmail.com>
2023-07-13 16:56:19 +0200

    registration form and more

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Création d’un utilisateur de type “contributeur” (= auteur)
+        $contributor = new User();
+        $contributor->setEmail('contributor@monsite.com');
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $contributor,
+            'contributorpassword'
+        );
+        $contributor->setFirstname('Loïc');
+        $contributor->setLastname('Pops');
+        $contributor->setIdentifiant('surnom');
+        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+        $contributor->setIsVerified(true);
+        $contributor->setPassword($hashedPassword);
+        $manager->persist($contributor);
+
+
+        $admin = new User();
+        $admin->setEmail('admin@monsite.com');
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $admin,
+            'adminpassword'
+        );
+        $admin->setFirstname('Loïc2');
+        $admin->setLastname('Pops2');
+        $admin->setIdentifiant('surnom2');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setIsVerified(true);
+        $admin->setPassword($hashedPassword);
+        $manager->persist($admin);
+
+
+        // Création d’un utilisateur de type “administrateur”
+        // $admin = new User();
+        // $admin->setEmail('admin@monsite.com');
+        // $admin->setRoles(['ROLE_ADMIN']);
+        // $hashedPassword = $this->passwordHasher->hashPassword(
+        //     $admin,
+        //     'adminpassword'
+        // );
+        // $admin->setPassword($hashedPassword);
+        // $manager->persist($admin);
+
+        // Sauvegarde des 2 nouveaux utilisateurs :
+        $manager->flush();
+    }
+}
+    // public const USER =
+    // [
+    //     [
+    //         'email' => 'info@afac.com',
+    //         'password' => '1234',
+    //         'firstname' => 'Loïc',
+    //         'lastname' => 'Lozok',
+    //         'identifiant' => 'aaaaa',
+    //         'role' => 'ROLE_USER'
+    //     ],
+
+    // ];
+    // public function load(ObjectManager $manager): void
+    // {
+        // foreach (self::USER as $userName => $value) {
+        //     $user = new User();
+        //     $user->setEmail('email']);
+        //     $user->setPassword('password']);
+        //     $user->setFirstname('firstname']);
+        //     $user->setLastname('lastname']);
+        //     $user->setIdentifiant('identifiant']);
+        //     $user->setRoles('role']);
+        //     $manager->persist($user);
+        // }
+        // $manager->flush();
---
+++ src/Entity/User.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Boolean;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $lastname = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $identifiant = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $isVerified = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getIdentifiant(): ?string
+    {
+        return $this->identifiant;
+    }
+
+    public function setIdentifiant(string $identifiant): self
+    {
+        $this->identifiant = $identifiant;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = [
+            'ROLE_CONTRIBUTOR', 'ROLE_USER'
+        ];
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getIsVerified(): ?bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+}
