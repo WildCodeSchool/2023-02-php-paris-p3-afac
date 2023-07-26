3a5fab71742599a2018ed64e6b09b14ab65faefc
Vincent C <vinch.cm@gmail.com>
2023-07-26 17:05:51 +0200

    last fixes CSS

--- migrations/Version20230726150028.php
+++ migrations/Version20230726150028.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230725144814 extends AbstractMigration
+final class Version20230726150028 extends AbstractMigration
 {
     public function getDescription(): string
     {
